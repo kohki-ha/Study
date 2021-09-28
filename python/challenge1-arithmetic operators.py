dda15b7cece632c7315728477378719e028c365b
Thales Kohki Hasegawa <kohkihasegawa151@gmail.com>
2021-09-28 20:17:56 -0300

    Adding python study folder

---
+++ python/challenge1-arithmetic operators.py
@@ -0,0 +1,14 @@
+salary: float
+expenditure: float
+
+salary = 3450.45
+expenditure = 2456.2
+
+print(f"salary = {salary}")
+print(f"expenditure = {expenditure:.2f}")
+
+percentage = (100 * expenditure) / salary
+
+print(f"percentage = {percentage:.2f}",end='')
+
+print(f"\npercentage = {(100 * expenditure) / salary:.2f}")
